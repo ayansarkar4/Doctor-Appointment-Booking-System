c1fe664278b3729f690f83dc99a605fd6d5643e4
Ayan Sarkar <146057122+ayansarkar4@users.noreply.github.com>
2026-01-06 12:32:48 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext();
 const AppContextProvider = ({ children }) => {
   const currency = "$";
 
-  const backendURL = "http://localhost:3500";
+  const backendURL = "https://doctor-appointment-booking-system-cfnn.onrender.com/";
 
   const [doctors, setDoctors] = useState([]);
   const [token, setToken] = useState(
